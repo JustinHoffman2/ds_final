482684a63f5b73f04dc338022c436d7b8f183fcd
Kyle Forsberg <kyle.forsberg@marquette.edu>
2025-05-04 19:11:03 -0500

    made confusion matrix for the beans KNN

--- models.R
+++ models.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(tidymodels)
+
 tracks_csv <- read_csv('./data/tracks.csv', show_col_types = FALSE)
 charts <- read_csv('./data/charts.csv', show_col_types = FALSE)
 
@@ -26,8 +27,9 @@ inside <- tracks_csv |>
 set.seed(100)
 outside <- tracks_csv |>
   filter(!track_name %in% charts$song) |>
-  sample_n(8918) |>
+  sample_n(2000) |>
   mutate(top_100 = 0)
+
 
 # Combine data frames
 tracks <- rbind(inside, outside)
@@ -61,17 +63,63 @@ library(tidymodels)
 tracks_filtered$duration_ms <- as.factor(tracks_filtered$duration_ms)
 tracks_filtered$top_100 <- as.factor(tracks_filtered$top_100)
 
+data_split <- initial_split(tracks_filtered, prop = 0.1, strata = top_100)  # stratify if classification
+df_trn <- training(data_split)
+df_test <- testing(data_split)
 
-knn_recipe <- recipe(top_100 ~ duration_ms, data = tracks_filtered) |> 
-  step_normalize(all_numeric_predictors())
+library(dplyr)
+
+test_small <- sample_n(df_test, 1500)
+
+
+
+knn_recipe <- recipe(top_100 ~ duration_ms + explicit + danceability + energy +
+                       loudness + speechiness + acousticness + liveness +
+                       valence + track_genre,
+                     data = df_trn) |>
+  step_dummy(all_nominal_predictors()) |>  # encode factors like track_genre
+  step_normalize(all_numeric_predictors()) 
+
 knn_mdl <- nearest_neighbor(mode = "classification", neighbors = 3)
+
+
 
 knn_out <- workflow() |> 
   add_recipe(knn_recipe) |> 
   add_model(knn_mdl) |> 
-  fit(data = tracks_filtered)
+  fit(data = df_trn)
+
+preds <- predict(knn_out, new_data = test_small) |> 
+  bind_cols(test_small)
+
+library(yardstick)
+
+accuracy(preds, truth = top_100, estimate = .pred_class)
+cm <- conf_mat(preds, truth = top_100, estimate = .pred_class)
 
 
+metrics(preds, truth = top_100, estimate = .pred_class)
+print(metrics)
 
+library(yardstick)
+library(ggplot2)
 
+# Compute the confusion matrix
+cm <- conf_mat(preds, truth = top_100, estimate = .pred_class)
 
+# Convert to data frame
+cm_df <- as.data.frame(cm$table)
+
+print(colnames(cm_df))
+
+ggplot(cm_df, aes(x = Prediction, y = Truth, fill = Freq)) +
+  geom_tile(color = "white") +
+  geom_text(aes(label = Freq), size = 5) +
+  scale_fill_gradient(low = "white", high = "steelblue") +
+  labs(
+    title = "Confusion Matrix",
+    x = "Predicted Class",
+    y = "Actual Class",
+    fill = "Count"
+  ) +
+  theme_minimal()
